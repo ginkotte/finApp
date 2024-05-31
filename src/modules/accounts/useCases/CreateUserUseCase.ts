c91b99ee33b74003b7baba215521ecdd31947a02
Gustavo Inkotte <gus.inkotte@gmail.com>
2024-05-31 01:04:38 -0300

    Fix database connections string. Create user endpoint

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,7 +1,21 @@
+import { prismaClient } from "../../../database/prismaClient";
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 
 class CreateUserUseCase {
-	async execute({ firstName, lastName, password, email }: ICreateUserDTO) {}
+	async execute({ firstName, lastName, password, email }: ICreateUserDTO) {
+		console.log(process.env.DATABASE_URL);
+
+		const user = await prismaClient.user.create({
+			data: {
+				firstName,
+				lastName,
+				password,
+				email,
+			},
+		});
+
+		return user;
+	}
 }
 
 export { CreateUserUseCase };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,6 @@ import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
-router.use("/users", usersRoutes)
+router.use("/user", usersRoutes);
 
 export { router };
